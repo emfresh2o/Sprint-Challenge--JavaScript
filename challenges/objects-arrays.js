77d7dc9bbe17500442f68a075d4ed09af8fff685
Ella Mae Freshwater <emfresh2o@Ellas-MacBook-Pro.local>
2020-05-02 14:41:33 -0400

    upload final changes

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,30 +5,42 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
+class dinosaurs{
+  constructor(obj){
+    this.name = obj.name;
+    this.diet = obj.diet;
+    this.weight = obj.weight;
+    this.length = obj.length;
+    this.period = obj.period;
+  }
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const tRex = {name: "tyrannosaure", diet: "carnivorous", weight: "7000kg", length: "12m", period: "Late Cretaceous"};
+// stegoSaur, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosarus = {name: "stegosaurus", diet: "herbivorous", weight: "2000kg", length: "9m", period: "Late Jurassic"};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const vRaptor = {name: "velociraptor", diet: "carnivorous", weight: "15kg", length: "1.8m", period: "Late Cretaceous"};
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tRex, weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(vRaptor, diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegoSaur, lenght);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tRex, period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tRex.roar = function(){
+  return "RAWERSRARARWERSARARARRRR!"
+}
+
+console.log(tRex.roar());
 
 
 // ==== Arrays ====
@@ -52,6 +64,11 @@ const graduates = [
 
 Once you have the new array created, log the result. */
 const universities = [];
+
+for(let i=0; i<graduates.lenght; i++){
+  universities[i] = graduates[i]["university"]
+}
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -61,10 +78,21 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for(let i=0; i<graduates.lenght; i++){
+  contactInfo[i] = `${graduates[i][first_name]} ${graduates[i][email]}`;
+}
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
+for(let i=0; i<graduates.lenght; i++){
+  if(graduatesw[i]["university"].includes('Uni')){
+    uni.push(graduates[i]["university"]);
+  }
+}
 console.log(unisWithUni);
 
 
@@ -91,6 +119,7 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach((animal) => displayNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`));
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -100,6 +129,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map((animal) => animal.animal_name.toLowerCase()).forEach((animal) => lowCaseAnimalNames.push(animals));
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -108,6 +138,7 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+zooAnimals.filter((animal) => animal.population < 5).forEach((animal) => lowPopulationAnimals.push(animals));
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -116,6 +147,9 @@ The zoos need to know their total animal population across the United States. Fi
 
 */
 const populationTotal = 0;
+
+populationTotal += zooAnimals.reduce((acc, animal) => acc + animal.population, 0);
+
 console.log(populationTotal);
 
 
